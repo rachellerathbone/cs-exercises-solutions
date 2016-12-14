eced21f1d39985ffd1068b796abfeba5c72718b0
Ken McGrady <ken.mcgrady@gmail.com>
2016-12-14 15:08:13 -0800

    Update Solutions

--- algorithms/search_algorithms.js
+++ algorithms/search_algorithms.js
@@ -11,7 +11,22 @@ const linearSearch = function(array, value) {
 }
 
 const binarySearch = function(array, value) {
+  let lo = 0;
+  let hi = array.length - 1;
 
+  while (lo <= hi) {
+    let mid = Math.floor(lo + (hi - lo) / 2);
+
+    if (array[mid] === value) {
+      return mid
+    } else if (array[mid] < value) {
+      lo = mid + 1;
+    } else {
+      hi = mid - 1;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = { linearSearch, binarySearch };
--- algorithms/sort_algorithms.js
+++ algorithms/sort_algorithms.js
@@ -1,35 +1,93 @@
 'use strict'
 
 function swap(arr, idx1, idx2) {
-
+  let temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
 }
 
-function bubbleSort(arr) {
+function bubbleSort(arr){
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
 
+  return arr;
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    if (i !== min) swap(arr, i, min);
+  }
 
+  return arr;
 }
 
 function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let currentValue = arr[i];
+    for (var j = i - 1; j > -1 && arr[j] > currentValue; j--) {
+      arr[j+1] = arr[j];
+    }
+    arr[j+1] = currentValue;
+  }
 
+  return arr;
 }
 
 function merge(arr1, arr2) {
+  let result = [];
+  let idx1 = 0;
+  let idx2 = 0;
 
+  while (idx1 < arr1.length && idx2 < arr2.length) {
+    result.push(arr1[idx1] < arr2[idx2] ? arr1[idx1++] : arr2[idx2++]);
+  }
+
+  return result.concat(arr1.slice(idx1)).concat(arr2.slice(idx2));
 }
 
-function partition(arr, left, right) {
+function partition(arr, left, right){
+  let pivotValue = arr[left];
+  let partitionIndex = left;
 
+  for(let i = left+1; i <= right; i++){
+    if(arr[i] < pivotValue){
+      partitionIndex++;
+      swap(arr, i, partitionIndex);
+    }
+  }
+
+  swap(arr, left, partitionIndex);
+
+  return partitionIndex;
 }
 
 function mergeSort(arr) {
+  if (arr.length < 2) return arr;
 
+  let mid = Math.floor(arr.length / 2);
+  let leftArr = arr.slice(0, mid);
+  let rightArr = arr.slice(mid);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
 }
 
-function quickSort(arr) {
+function quickSort(arr, left = 0, right = arr.length - 1){
+  if(left < right){
+   let partitionIndex = partition(arr, left, right);
+   quickSort(arr, left, partitionIndex - 1);
+   quickSort(arr, partitionIndex + 1, right);
+  }
 
+  return arr;
 }
 
 module.exports = {
